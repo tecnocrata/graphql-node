0e6bb42a0aa2468c5dcb325b1bf34a6fc9fc8530
Enrique Ortuno <enrique@ortuno.net>
2021-02-27 12:25:06 -0400

    standardjs installed and configured

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const { graphql, buildSchema } = require("graphql");
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Definiendo schema
 const schema = buildSchema(`
 type Query {
   "Retorna saludo al mundo"
   hello: String
-  "Retorna un saludoa todos"
+  "Retorna un saludo a todos"
   greetings: String
 }
-`);
+`)
 
 // Configurar los resolvers
 const resolvers = {
   hello: () => {
-    return "Hello world";
+    return 'Hello world'
   },
   greetings: () => {
-    return "How are you?";
-  },
-};
+    return 'How are you?'
+  }
+}
 
 app.use(
-  "/api",
+  '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true, // This is the ide for testing
+    graphiql: true // This is the ide for testing
   })
-);
+)
 
 app.listen(PORT, () => {
-  console.log(`Server listen on port http://localhost:${process.env.PORT}/api`);
-});
+  console.log(`Server listen on port http://localhost:${process.env.PORT}/api`)
+})
 
 // // Ejecutar el primer Hello
 // graphql(schema, "{ greetings }", resolvers).then((data) => {
