8dee73aae7ad96e609b1ee8a002f0bdf7133e396
Enrique Ortuno <enrique@ortuno.net>
2021-03-14 12:48:58 -0400

    production 'ready'

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
+const cors = require('cors')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-
+const isProduction = process.env.NODE_ENV === 'production'
 // Definiendo schema
 const typeDefs = readFileSync(
   join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+app.use(cors())
 app.use(
   '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true // This is the ide for testing
+    graphiql: !isProduction // This is the ide for dev/testing, NOT production
   })
 )
 
